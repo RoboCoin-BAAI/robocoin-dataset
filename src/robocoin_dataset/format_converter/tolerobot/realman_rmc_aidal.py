6fa9d8ca9c84c382e6332832f6d27d2caaab200c
Liuyiheng <18092288389@163.com>
2025-09-09 13:39:34 +0800

    fix

--- src/robocoin_dataset/format_converter/tolerobot/realman_rmc_aidal.py
+++
@@ -1,8 +0,0 @@
-from robocoin_dataset.format_convertors.tolerobot.lerobot_format_convertor import (
-    LerobotFormatConvertor,
-)
-
-
-class RealmanRmcAidal(LerobotFormatConvertor):
-    def __init__(self, dataset_path: str, lerobot_dst_path: str) -> None:
-        super().__init__(dataset_path, lerobot_dst_path)
